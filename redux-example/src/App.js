cac944202e41742a82093f1cbee26c63f5acfdc3
karatugo <karatugo@gmail.com>
2020-07-03 14:38:50 +0300

    Introduce Redux to the app

--- redux-example/src/App.js
+++ redux-example/src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
 
 import Posts from './components/Posts'
 import PostForm from './components/PostForm'
 
+import store from './store'
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <PostForm />
-                <hr />
-                <Posts />
-            </header>
-        </div>
+        <Provider store={store}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <PostForm />
+                    <hr />
+                    <Posts />
+                </header>
+            </div>
+        </Provider>
     );
 }
 
--- redux-example/src/components/PostForm.js
+++ redux-example/src/components/PostForm.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { createPost } from '../actions/postAction'
+import { createStore } from 'redux'
 
 class PostForm extends Component {
     constructor(props) {
@@ -24,15 +28,8 @@ class PostForm extends Component {
             body: this.state.body
         }
 
-        fetch('https://jsonplaceholder.typicode.com/posts',{
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(post)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
+
+        this.props.createPost(post)
     }
 
     render() {
@@ -57,4 +54,9 @@ class PostForm extends Component {
     }
 }
 
-export default PostForm
+PropTypes.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+
+export default connect(null, { createPost })(PostForm)
